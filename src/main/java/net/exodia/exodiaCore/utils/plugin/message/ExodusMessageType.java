cd73e7865c3be626a21a41e52b2d68922be073df
Dqvidd <david.alert.f@gmail.com>
2024-08-21 23:46:55 +0200

    Fix PluginUtils

--- src/main/java/net/exodia/exodiaCore/utils/plugin/message/ExodusMessageType.java
+++ src/main/java/net/exodia/exodiaCore/utils/plugin/message/ExodusMessageType.java
@@ -1,14 +1,14 @@
 package net.exodia.exodiaCore.utils.plugin.message;
 
-import net.exodia.exodiaCore.utils.Utils;
+import net.exodia.exodiaCore.utils.plugin.PluginUtils;
 
 public enum ExodusMessageType {
 
     /* Enumeración para gestionar los tipos de mensajes que se pueden enviar por consola */
 
-    WARN(Utils.PREFIX + "&8[&eWarn&8] &7"),
-    DEBUG(Utils.PREFIX + "&8[&2Debug&8] &7"),
-    ERROR(Utils.PREFIX + "&8[&cError&8] &7");
+    WARN(PluginUtils.PREFIX + "&8[&eWarn&8] &7"),
+    DEBUG(PluginUtils.PREFIX + "&8[&2Debug&8] &7"),
+    ERROR(PluginUtils.PREFIX + "&8[&cError&8] &7");
 
     private final String prefix;
 
