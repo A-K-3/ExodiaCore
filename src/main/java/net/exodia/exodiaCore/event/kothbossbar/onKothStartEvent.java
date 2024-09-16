e748a5103bc49956e10d011317592a89aaf5da8f
Dqvidd <david.alert.f@gmail.com>
2024-09-17 01:03:01 +0200

    Bossbar logic

--- src/main/java/net/exodia/exodiaCore/event/kothbossbar/onKothStartEvent.java
+++ src/main/java/net/exodia/exodiaCore/event/kothbossbar/onKothStartEvent.java
@@ -1,35 +1,100 @@
 package net.exodia.exodiaCore.event.kothbossbar;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import me.ulrich.koth.Koth;
 import me.ulrich.koth.api.KothAPIManager;
 import me.ulrich.koth.data.KothData;
+import me.ulrich.koth.events.ExtEvent;
 import me.ulrich.koth.events.KothCaptureEvent;
+import me.ulrich.koth.events.KothEndEvent;
+import me.ulrich.koth.events.KothStartEvent;
 import net.exodia.exodiaCore.event.ExodiaEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.boss.BossBar;
-import org.bukkit.boss.*;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.UUID;
 
+public class onKothStartEvent extends ExodiaEvent implements Listener {
 
-public class onKothStartEvent extends ExodiaEvent {
-
-    private KothAPIManager kothAPIManager;
     private BossBar bossbar;
+
+    @EventHandler
+    public void kothStartEnd(KothStartEvent e) {
+        UUID kothUUID = e.getKothUUID();
+        String kothname = plugin.kothAPIManager.getKothName(kothUUID).get();
+
+        // Crear y mostrar la BossBar cuando comience la captura del KOTH
+        if (bossbar != null) {
+            // Eliminar la BossBar anterior si existe
+            bossbar.removeAll();
+        }
+        bossbar = Bukkit.createBossBar("Capturando KOTH: " + kothname, BarColor.RED, BarStyle.SOLID);
+        bossbar.setVisible(true);
+
+        // Mostrar la nueva BossBar a todos los jugadores
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            bossbar.addPlayer(onlinePlayer);
+        }
+
+        // Iniciar la verificación periódica del estado del KOTH
+        startMonitoring(kothname);
+    }
 
     @EventHandler
     public void kothBossBar(KothCaptureEvent e) {
         UUID kothUUID = e.getKothUUID();
-        String player = e.getPlayer().getName();
         String kothname = plugin.kothAPIManager.getKothName(kothUUID).get();
+        startMonitoring(kothname);
+    }
 
+    @EventHandler
+    public void onKothEnd(KothEndEvent e) {
+        if (bossbar != null) {
+            bossbar.removeAll();
+            bossbar = null;
+        }
+    }
 
-        System.out.println("start bossbar");
-        System.out.println(player + " está capturando el KOTH " + kothname);
+    private void startMonitoring(String kothname) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                updateBossBar(kothname);
+            }
+        }.runTaskTimer(plugin, 0L, 20L); // Revisa cada segundo (20 ticks)
+    }
 
-// TODO CONFIG
+    private void updateBossBar(String kothname) {
+        if (bossbar != null) {
+            bossbar.removeAll();
+
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                String kothController = PlaceholderAPI.setPlaceholders(player, "%ukoth_{" + kothname + "}_control_player%");
+                String kothTimeLeft = PlaceholderAPI.setPlaceholders(player, "%ukoth_{" + kothname + "}_timeleft%");
+                String kothDefaultTime = PlaceholderAPI.setPlaceholders(player, "%ukoth_{" + kothname + "}_capture_time_formated%");
+
+                if (kothController != null && !kothController.isEmpty() && !kothController.equalsIgnoreCase("§cNinguno")) {
+                    String title = "Capturando KOTH: " + kothController + " - Tiempo restante: " + kothTimeLeft;
+                    bossbar.setTitle(title);
+                    // TODO: Hacer que el progress vaya en función del tiempo restante %ukoth_{end}_timeleft%
+                    bossbar.setProgress(1);
+                    break;
+                } else {
+                    String title = "Capturando KOTH: Nadie - Tiempo restante: " + kothDefaultTime;
+                    bossbar.setTitle(title);
+                    bossbar.setProgress(0.02);
+                }
+            }
+
+            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                bossbar.addPlayer(onlinePlayer);
+            }
+        }
     }
 }
